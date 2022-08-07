1a5522c84a5571fea0ea1f22a1a8724da2ff60f4
Filip Tijan <ftijan@gmail.com>
2022-08-07 17:15:07 +0100

    add used method implementations

---
+++ jqueryClone.js
@@ -0,0 +1,63 @@
+class ElementCollection extends Array {
+    ready(cb) {
+        const isReady = this.some(e => {
+            return e.readyState != null && e.readyState != 'loading'
+        })
+        if(isReady) {
+            cb()
+        } else {
+            this.on('DOMContentLoaded', cb)            
+        }
+        return this
+    }
+
+    on(event, cbOrSelector, cb) {
+        if (typeof cbOrSelector === 'function') {
+            this.forEach(e => e.addEventListener(event, cbOrSelector))
+        } else {
+            this.forEach(elem => {
+                elem.addEventListener(event, e => {
+                    if (e.target.matches(cbOrSelector)) cb(e)
+                })
+            })
+        }
+        return this
+    }
+
+    next() {
+        return this.map(e => e.nextElementSibling).filter(e => e != null)
+    }
+
+    prev() {
+        return this.map(e => e.previousElementSibling).filter(e => e != null)
+    }
+
+    removeClass(className) {
+        this.forEach(e => e.classList.remove(className))
+        return this
+    }
+
+    addClass(className) {
+        this.forEach(e => e.classList.add(className))
+        return this
+    }
+
+    css(property, value) {
+        // Should not be necessary:
+        // const camelProp = property.replace(/(-[a-z])/, g => {
+        //     return g.replace('-', '').toUpperCase()
+        // })
+        //this.forEach(e => e.style[camelProp] = value)
+        this.forEach(e => e.style[property] = value)
+        return this
+    }
+}
+
+function $(param) {
+    if (typeof param === 'string' || param instanceof String)
+    {
+        return new ElementCollection(...document.querySelectorAll(param))
+    } else {
+        return new ElementCollection(param)
+    }
+}
